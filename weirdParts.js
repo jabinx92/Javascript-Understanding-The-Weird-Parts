2eca0e4637b1550135aadbe142f39f56f66a270e
Jonathan Won <jabinx92@gmail.com>
2020-02-20 22:57:58 -0800

    global environment

--- weirdParts.js
+++ weirdParts.js
@@ -1,1 +1,25 @@
-//hello
+/**
+ * Big Words and Javascript
+ */
+
+ /* Everytime the lectures go, the instructor will pause and mention a "Big word alert". Explaining the word. Show how the word is used. It is just vocabulary
+ */
+
+
+ /**
+ * Conceptual Aside
+ */
+
+ /* 
+Syntax parsers - a program that reads your code and detemines what id does and if its grammar is valid. - Your code isn't magic, Someone else wrote a program to translate it for the computer.
+
+Lexical enviroment - where something sits physically in the code you write. "Lexical" means having to do with words or grammar. A lexical enviroment exists in programming languages in which where you write something is important.
+
+Execution context - a wrapper to help manage the code that is running. There are lots of lexical environments. Which one is currently running is managed via execution contexts. It can contain things beyond what you've written in your code.
+
+Name/Value Pair: A name which maps to a unique value. The name may be defined more than once, but only can have one value in any given context. That value may be more name/value pairs.
+
+Address = '100 Main St.' Address is the name, value is the street.
+
+Objection: a collection of name value pairs. Most simple definition when talking about javascript.
+ */
